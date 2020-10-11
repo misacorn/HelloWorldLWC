5e4932dad879cd58729094b17b133a7576b2f83d
Misa <dieuhuongngo@gmail.com>
2020-10-11 15:20:41 +0300

    decorators

--- force-app/main/default/lwc/ebike/ebike.js
+++ force-app/main/default/lwc/ebike/ebike.js
@@ -1,17 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class Ebike extends LightningElement {
-  name = "Electra X4";
-  description = "A sweet bike built for comfort.";
-  category = "Mountain";
-  material = "Steel";
-  price = "$2,700";
-  pictureUrl =
-    "https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg";
-  ready = false;
-  connectedCallback() {
-    setTimeout(() => {
-      this.ready = true;
-    }, 3000);
-  }
+  // name = "Electra X4";
+  // description = "A sweet bike built for comfort.";
+  // category = "Mountain";
+  // material = "Steel";
+  // price = "$2,700";
+  // pictureUrl =
+  //   "https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg";
+  // ready = false;
+  // connectedCallback() {
+  //   setTimeout(() => {
+  //     this.ready = true;
+  //   }, 3000);
+  // }
+  @api bike;
 }
